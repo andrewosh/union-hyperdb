71c4597e885c3cac3666233a36377b403b9362bc
Andrew Osheroff <andrewosh@gmail.com>
2018-02-16 19:36:31 -0800

    Simple gets/puts work

--- test/basic.js
+++ test/basic.js
@@ -23,3 +23,36 @@ test('put/get with a single layer', function (t) {
     })
   })
 })
+
+test('put/get with two layers', function (t) {
+  t.plan(10)
+
+  create.fromLayers([
+    [
+     { type: 'put', key: 'a', value: 'hello' },
+     { type: 'put', key: 'b', value: 'goodbye' }
+    ],
+    [
+     { type: 'put', key: 'a', value: 'dog' },
+     { type: 'put', key: 'c', value: 'human' }
+    ]
+  ], function (err, db) {
+    t.error(err)
+    db.get('a', function (err, nodes) {
+      t.error(err)
+      t.same(nodes.length, 1)
+      t.same(nodes[0].value, 'dog')
+      db.get('b', function (err, nodes) {
+        t.error(err)
+        t.same(nodes.length, 1)
+        t.same(nodes[0].value, 'goodbye')
+        db.get('c', function (err, nodes) {
+          t.error(err)
+          t.same(nodes.length, 1)
+          t.same(nodes[0].value, 'human')
+        })
+      })
+    })
+  })
+
+})
