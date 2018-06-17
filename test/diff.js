447a4f7d1f5fa37e1fd86bfecf13b341cf7edbe8
Andrew Osheroff <andrewosh@gmail.com>
2018-06-17 02:46:58 -0700

    Added missing diff tests

---
+++ test/diff.js
@@ -0,0 +1,52 @@
+var test = require('tape')
+var equals = require('shallow-equals')
+
+var create = require('./helpers/create')
+
+test('simple diff, no links', t => {
+  create.fromLayers([
+    [
+      { type: 'put', key: 'a', value: 'hello' },
+      { type: 'put', key: 'b', value: 'goodbye' }
+    ]
+  ], function (err, db) {
+    t.error(err)
+    validate(db)
+  })
+
+  var expected = [
+    { left: 'a', right: null },
+    { left: 'b', right: null }
+  ]
+  var seen = 0
+
+  function validate (db) {
+    var diff = db.createDiffStream(null, '/')
+    diff.on('data', (diff) => {
+      for (var i = 0; i < expected.length; i++) {
+        if (equals(toKeys(diff), expected[i])) {
+          seen++
+          return
+        }
+      }
+      t.fail('unexpected diff:', diff)
+    })
+    diff.on('end', () => {
+      t.equals(expected.length, seen)
+      t.end()
+    })
+    diff.on('error', (err) => {
+      t.fail(err.message)
+      t.end()
+    })
+  }
+
+})
+
+function toKeys (diff) {
+  return {
+    left: diff.left ? diff.left[0].key : null,
+    right: diff.right ? diff.right[0].key : null
+  }
+}
+
